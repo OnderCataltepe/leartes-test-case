fef7db006da4b746c0b6e2c9b255929c2a4dd60f
OnderCataltepe <ondercataltepe@gmail.com>
2023-03-20 22:59:01 +0300

    add json-server base-url

--- src/api/httpRequests.ts
+++ src/api/httpRequests.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosResponse } from 'axios';
-
+const JSON_SERVER_BASE_URL = 'http://localhost:3004/';
+const GLITCH_API_BASE_URL = 'https://flash-cherry-ceder.glitch.me/';
 const API = axios.create({
-  baseURL: 'https://flash-cherry-ceder.glitch.me/',
+  baseURL: GLITCH_API_BASE_URL,
   timeout: 1000,
   headers: {
     Accept: 'application/json'
