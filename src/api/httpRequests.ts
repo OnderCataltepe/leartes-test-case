7bb131c5d9b4702f7197360ee48ef14f819ef5a9
OnderCataltepe <ondercataltepe@gmail.com>
2023-03-21 15:34:27 +0300

    disable timeout

--- src/api/httpRequests.ts
+++ src/api/httpRequests.ts
@@ -3,7 +3,6 @@ const JSON_SERVER_BASE_URL = 'http://localhost:3004/';
 const GLITCH_API_BASE_URL = 'https://flash-cherry-ceder.glitch.me/';
 const API = axios.create({
   baseURL: GLITCH_API_BASE_URL,
-  timeout: 1000,
   headers: {
     Accept: 'application/json'
   }
